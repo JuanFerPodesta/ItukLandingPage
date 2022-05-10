21b071fcfbf04c99bdfa42548ee89d78d608a170
JuanFerPodesta <juan.fernando.podesta@gmail.com>
2022-05-09 21:23:58 -0300

    uploading changes

--- app/js/script.js
+++ app/js/script.js
@@ -3,21 +3,43 @@ const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElements = document.querySelectorAll(".has-fade");
+const linksMobile = document.querySelectorAll(".linksMobile");
+
+window.addEventListener("resize", function () {
+  if (header.classList.contains("open")) {
+    cerrarBoton();
+  }
+});
 
 btnHamburger.addEventListener("click", function () {
   if (header.classList.contains("open")) {
-    header.classList.remove("open");
-    body.classList.remove("noscroll");
-    fadeElements.forEach(function (element) {
-      element.classList.remove("fade-in");
-      element.classList.add("fade-out");
-    });
+    cerrarBoton();
   } else {
-    header.classList.add("open");
-    body.classList.add("noscroll");
-    fadeElements.forEach(function (element) {
-      element.classList.remove("fade-out");
-      element.classList.add("fade-in");
-    });
+    abrirBoton();
+    for (i = 0; i < linksMobile.length; i++) {
+      linksMobile[i].addEventListener("click", cerrarBoton);
+    }
   }
 });
+
+// *** funciones hamburguerBtn ***
+
+function cerrarBoton() {
+  header.classList.remove("open");
+  body.classList.remove("noscroll");
+  fadeElements.forEach(function (element) {
+    element.classList.remove("fade-in");
+    element.classList.add("fade-out");
+  });
+}
+
+function abrirBoton() {
+  header.classList.add("open");
+  body.classList.add("noscroll");
+  fadeElements.forEach(function (element) {
+    element.classList.remove("fade-out");
+    element.classList.add("fade-in");
+  });
+}
+
+// *** fin funciones hamburguerBtn ***
