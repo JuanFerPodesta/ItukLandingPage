f3a8570e7e925e78be7b6c732a776a5808803829
JuanFerPodesta <juan.fernando.podesta@gmail.com>
2022-05-13 00:22:54 -0300

    uploading changes

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,24 @@
-const btnHamburger = document.querySelector("#btnHamburger");
+// sections
+
 const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+
+const hero = document.getElementById("hero");
+
 const fadeElements = document.querySelectorAll(".has-fade");
 const linksMobile = document.querySelectorAll(".linksMobile");
 
+// btns
+const btnHamburger = document.querySelector("#btnHamburger");
+
 window.addEventListener("resize", function () {
   if (header.classList.contains("open")) {
-    cerrarBoton();
+    cerrarBotoRapido();
+    mostrar(hero);
+  }
+  if (document.documentElement.scrollTop > 0) {
+    cerrarBotoRapido();
   }
 });
 
@@ -22,9 +33,20 @@ btnHamburger.addEventListener("click", function () {
   }
 });
 
-// *** funciones btnHamburger ***
+// * btnHamburger functions *
+
+function cerrarBotoRapido() {
+  header.classList.remove("open");
+  body.classList.remove("noscroll");
+  fadeElements.forEach(function (element) {
+    ocultar(element);
+  });
+}
 
 function cerrarBoton() {
+  if (document.documentElement.scrollTop === 0) {
+    mostrar(hero);
+  }
   header.classList.remove("open");
   body.classList.remove("noscroll");
   fadeElements.forEach(function (element) {
@@ -34,12 +56,36 @@ function cerrarBoton() {
 }
 
 function abrirBoton() {
+  if (document.documentElement.scrollTop > 0) {
+    ocultar(hero);
+  }
   header.classList.add("open");
   body.classList.add("noscroll");
   fadeElements.forEach(function (element) {
+    mostrar(element);
     element.classList.remove("fade-out");
     element.classList.add("fade-in");
   });
 }
 
-// *** fin funciones btnHamburger ***
+// *** end of btnHamburger functions ***
+
+// * general functions *
+
+function ocultar(section) {
+  section.classList.add("hidden");
+}
+
+function mostrar(section) {
+  section.classList.remove("hidden");
+}
+
+function setBackToDefault(valor) {
+  valor.value = ``;
+}
+
+function setBackToDefaultElement(valor) {
+  valor.innerHTML = ``;
+}
+
+// *** end of general functions ***
